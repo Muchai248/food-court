2df2388182591fb72fc6d31057396cf9d8d5223c
Mark Muchai <muchaimark6@gmail.com>
2023-08-01 14:01:15 +0300

    data commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import LandingPage from './components/LandingPage';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef} from 'react';
 import './App.css';
 
 function App() {
 
   const [restaurant, setRestaurant]=useState([])
   const [currentindex, setCurrentIndex]=useState(0)
+  const newdata=useRef()
+
+
 
   useEffect(()=>{
     fetch("https://my-json-server.typicode.com/Muchai248/Muchai-248-restaurant/db")
     .then((r)=>r.json())
     .then((data)=>{
       setRestaurant(data)
+      newdata.current=data
       // console.log(data)
     })
     
@@ -22,7 +26,7 @@ function App() {
   return (
     <>
     <div>
-      <LandingPage currentindex={currentindex} setCurrentIndex={setCurrentIndex} restaurant={restaurant} />
+      <LandingPage currentindex={currentindex} setCurrentIndex={setCurrentIndex} restaurant={restaurant}  newdata={newdata}/>
     </div>
     </>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,14 @@
+import { useState } from "react"
 import { BrowserRouter } from "react-router-dom"
 
-export default function LandingPage({restaurant, setRestaurant, currentindex,  setCurrentIndex}){
-    const current=[
-        1,2,3,4
-    ]
+export default function LandingPage({restaurant, setRestaurant, currentindex,  setCurrentIndex, newdata}){
+   const [data, setData]=useState({...restaurant[0]})
+    // console.log(restaurant.restaurants)
 
+    // console.log(newdata.current.restaurants)
 
 function handleLeft(){
-    console.log(restaurant)
+    // console.log(restaurant)
     if(currentindex>0){
      setCurrentIndex(currentindex-1)
     }
@@ -18,19 +19,20 @@ function handleRight(){
     if(currentindex<restaurant.length){
      setCurrentIndex(currentindex+1)
     }
-    console.log(currentindex)
+    // console.log(currentindex)
 }
 
     return(
         <div className="homepage">
             <img src="https://images.unsplash.com/photo-1543353071-873f17a7a088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" width="100%" height="100%"/>
-            <div className="homename">name</div>
-             <div className="homedescription"> "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-             "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-       </div>
+            {/* <div className="homename">{data.name}</div> */}
+             {/* <div className="homedescription">{data.description}
+       </div> */}
             <div className="arrowleft" onClick={handleLeft}> arrow left</div>
             <div className="arrowright" onClick={handleRight}> arrow right </div>
-            <div className="restimage"></div>
+            <div className="restimage"> 
+                {/* <img src={data.image}/>  */}
+                </div>
             <div className="circle"> 
                 <div className="indicator"> a </div>
                 <div className="indicator"> b</div>
