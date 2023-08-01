bc87735a25d93d815109be6a323c772b9435f360
Mark Muchai <muchaimark6@gmail.com>
2023-08-01 10:44:33 +0300

    initial Landingpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+import LandingPage from './components/LandingPage';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [restaurant, setRestaurant]=useState([])
+
+  useEffect(()=>{
+    fetch("https://my-json-server.typicode.com/Muchai248/Muchai-248-restaurant/db")
+    .then((r)=>r.json())
+    .then((data)=>{
+      setRestaurant(data)
+      // console.log(data)
+    })
+    
+
+  },[])
+
+
   return (
     <>
+    <div>
+      <LandingPage/>
+    </div>
     </>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import { BrowserRouter } from "react-router-dom"
+
+export default function LandingPage({restaurant, setRestaurant}){
+
+    return(
+        <div>
+            <img/>
+            <div>name </div>
+             <div> Description</div>
+            <div> arrow left </div>
+            <di> arrow right </di>
+            <div> 
+                <div> a </div>
+                <div> b</div>
+                <div> c</div>
+             </div>
+            
+        </div>
+    )
+
+ 
+}
